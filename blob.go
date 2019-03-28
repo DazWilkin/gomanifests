380b6fa044680070b452b35c0eee68736a155b75
Your Name <you@example.com>
2019-03-27 20:17:44 -0700

    Added SHA-256

--- blob.go
+++ blob.go
@@ -1,8 +1,14 @@
 package gomanifests
 
-import ()
+import (
+    "crypto/sha256"
+    "hash"
+)
 
 type Blob struct{}
 func (b *Blob) Layer() Layer {
     return Layer{}
+}
+func (b *Blob) Digest() hash.Hash {
+    return sha256.New()
 }
